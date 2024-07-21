1c4b9fb9ca9c42b35b70e63b5772e70e384c11d8
unknown <galor_nimrod@hotmail.com>
2024-07-21 15:56:35 +0300

    remove config

--- routes/configuration.js
+++ routes/configuration.js
@@ -3,7 +3,7 @@ import express from 'express'
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    res.redirect('configuration.html')
+    res.redirect('/admin/configuration.html')
 })
 
 export default router
--- server.js
+++ server.js
@@ -7,12 +7,12 @@ import cookieParser from 'cookie-parser'
 import session from 'express-session'
 import passport from 'passport'
 import logger  from 'morgan'
-import isConfigurationDone from './statico/configuration.js'
+
 
 import initialize from './statico/initialize.js'
 
 // routes
-import configRouter from './routes/configuration.js'
+
 import pagesRouter from './routes/pages.js'
 import userRouter from './routes/users.js'
 import authRouter from './routes/auth.js'
@@ -70,15 +70,8 @@ app.use(function(req, res, next){
     next()
 })
 
-function configMiddle(req, res, next){
-    if(!configurationDone){
-        return res.redirect('/configuration');
-    }
-    next()
-}
 // Routes
-app.use('/configuration', configRouter)
-app.use('/', configMiddle, pagesRouter)
+
 app.use('/admin', adminRouter)
 app.use('/users', userRouter)
 app.use('/', authRouter)
