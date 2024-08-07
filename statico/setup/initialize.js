71df642e10810e649c5c1f4e794ca1ad57a44ab0
unknown <galor_nimrod@hotmail.com>
2024-08-07 10:44:43 +0300

    ref initializ added permissions

--- statico/setup/initialize.js
+++ statico/setup/initialize.js
@@ -1,12 +1,11 @@
-// import fs from 'fs'
-
+ 
 import path from 'path'
 import { fileURLToPath } from 'url'
-import { unlink } from 'node:fs/promises'
+import fs, { unlink } from 'node:fs/promises'
 import { PrismaClient } from '@prisma/client'
-import { error } from 'console'
+// import { error } from 'console'
 import crypto from 'crypto'
-// import readRows, {createRow} from '../db.js'
+
 const prisma = new PrismaClient()
 
 export default async function initialize(email, emailVerified, password, userName){
@@ -18,58 +17,72 @@ export default async function initialize(email, emailVerified, password, userNam
         if(roles > 0){
             throw new Error('Installation has already been completed', "")
         }
-    }).then(async () => {
-        //  create subscribe Role
-        return await prisma.role.create({
-            data: {
+    }).then(async () => {// ****  Create Roles
+        // Update Progress
+        progress.push('Subscriber Role Created.', 'Contributer Role Created.', 'Author Role Created.', 'Editor Role Created.', 'Admin Role Created.')
+
+        // Role defenition
+        const roles = [
+            {
                 name: 'subscriber',
-                description: 'somebody who can only manage their profile.',
+                description: 'Somebody who can only manage their profile.',
                 default: true
-            }
-        })
-    })
-    .then(async () => {
-        progress.push('Subscribe Role Created.')
-        //  create contributor Role
-        return await prisma.role.create({
-            data: {
+            },
+            {
                 name: 'contributor',
-                description: 'somebody who can write and manage their own posts but cannot publish them.'
-            }
-        })
-    })
-    .then(async () => {
-        progress.push('Contributer Role Created.')
-        //  create author Role
-        await prisma.role.create({
-                data: {
+                description: 'Somebody who can write and manage their own posts but cannot publish them.'
+            },
+            {
                 name: 'author',
-                description: 'somebody who can publish and manage their own posts.'
-            }
-        })
-    })
-    .then(async () => {
-        progress.push('Author Role Created.')
-        //  create editor Role
-        return await prisma.role.create({
-            data: {
+                description: 'Somebody who can publish and manage their own posts.'
+            },
+            {
                 name: 'editor',
-                description: 'somebody who can publish and manage posts including the posts of other users.'
-            }
-        })
-    })
-    .then(async () => {
-        progress.push('Editor Role Created.')
-        //  create admin Role
-        return await prisma.role.create({
-            data: {
+                description: 'Somebody who can publish and manage posts including the posts of other users.'
+            },
+            {
                 name: 'admin',
-                description: 'somebody who has access to all the administration features within a single site.'
+                description: 'Somebody who has access to all the administration features within a single site.'
             }
-        })
+        ]
+
+        // create roles
+        const rolesPromise = []
+        for(let i=0; i<roles.length; i++){
+            rolesPromise.push(prisma.role.create({ data: roles[i]} ) )
+        }
+
+        const rolesCreated = await Promise.all(rolesPromise)
+
+        return rolesCreated
     })
-    .then(async (adminRole) => {
-        progress.push('Admin Role Created.')
+    .then(async (rolesCreated) => {// ****  Update permissions
+        // Update Progress
+        progress.push('Updated permissions.')
+
+        const __filename = fileURLToPath(import.meta.url);
+        const __dirname = path.dirname(__filename);
+        const defaultfilePath = path.join( __dirname, '..', 'admin', 'permissions', 'default.permissions.json')
+        // read permissions file
+        let permissionsString = await fs.readFile(defaultfilePath, { encoding: 'utf8' });
+        //  for each role replase role name with role id
+        for(let i = 0; i < rolesCreated.length; i++){
+            const regex = new RegExp(rolesCreated[i].name, "g")
+            permissionsString = permissionsString.replace(regex, rolesCreated[i].id)
+        }
+        // create new file with updated permissions
+        const filePath = path.join( __dirname, '..', 'admin', 'permissions', 'permissions.json')
+        
+        await fs.writeFile(filePath, permissionsString)
+        
+        const adminRole = rolesCreated[4]
+        // return admin role to create admin user
+        return adminRole
+    })
+    .then(async (adminRole) => {// ****  Create Admin user
+        // Update Progress
+        progress.push(`Admin user (${userName}) Created.`)
+
         // // hash passowrd
         const salt = crypto.randomBytes(16)
         const key = crypto.pbkdf2Sync(password, salt, 100000, 64, 'sha512');
@@ -87,10 +100,12 @@ export default async function initialize(email, emailVerified, password, userNam
         }
     
         await prisma.user.create({data: adminUser})
-        return
+        // return
     })
-    .then(async () => {
-        progress.push('Admin user Created.')
+    .then(async () => {// ****  Create Default Pages
+        // Update Progress
+        progress.push('Default Home page Created.')
+        
         // Create Default home page
         const homePage = {
             metatitle:  "Statico Default Home Page",
@@ -101,11 +116,12 @@ export default async function initialize(email, emailVerified, password, userNam
             publish: true
         }
         await prisma.page.create({data: homePage})
-        return
+        // return
     })
-    .then(async () => {
-        progress.push('Default Home page Created.')
-        // Create Default Features page
+    .then(async () => {// ****  Create Default Features page
+        // Update Progress
+        progress.push('Default Features page Created.')
+        
         const featuresPage = {
             metatitle:  "Statico Default Features Page",
             metadescription:  "Statico Default Features Page",
@@ -115,11 +131,12 @@ export default async function initialize(email, emailVerified, password, userNam
             publish: true
         }
         await prisma.page.create({data: featuresPage})
-        return
+        // return
     })
-    .then(async () => {
-        progress.push('Default Features page Created.')
-        // Create Default Pricing page
+    .then(async () => {// ****  Create Default Pricing page
+        // Update Progress
+        progress.push('Default Home pricing Created.')
+        
         const pricingPage = {
             metatitle:  "Statico Default Pricing Page",
             metadescription:  "Statico Default Pricing Page",
@@ -129,11 +146,12 @@ export default async function initialize(email, emailVerified, password, userNam
             publish: true
         }
         await prisma.page.create({data: pricingPage})
-        return
+        // return
     })
-    .then(async () => {
-        progress.push('Default Home pricing Created.')
-        // Create Default FAQ page
+    .then(async () => {// ****  Create Default FAQ page
+        // Update Progress
+        progress.push('Default FAQ page Created.')
+        
         const faqPage = {
             metatitle:  "Statico Default FAQ Page",
             metadescription:  "Statico Default FAQ Page",
@@ -143,11 +161,12 @@ export default async function initialize(email, emailVerified, password, userNam
             publish: true
         }
         await prisma.page.create({data: faqPage})
-        return
+        // return
     })
-    .then(async () => {
-        progress.push('Default FAQ page Created.')
-        // Create Default About page
+    .then(async () => {// ****  Create Default About page
+        // Update Progress
+        progress.push('Default About page Created.')
+        
         const aboutPage = {
             metatitle:  "Statico Default About Page",
             metadescription:  "Statico Default About Page",
@@ -157,17 +176,18 @@ export default async function initialize(email, emailVerified, password, userNam
             publish: true
         }
         await prisma.page.create({data: aboutPage})
-        return
+        // return
     })
     .then(async () => {
-        progress.push('Default About page Created.')
+        // Update Progress
+        progress.push('Setup completed successfully.')
+        
         //  if we got up to heare with no errors, delete setup (index) file from public folder.
         const __filename = fileURLToPath(import.meta.url);
         const __dirname = path.dirname(__filename);
         const filePath = path.join( __dirname, '..', '..', 'public', 'index.html')
         await unlink(filePath)
 
-        progress.push(`Successfully created Roles, Default pages and Admin user: "${userName}"`)
         return {message: progress, success: true}
     })
     .catch((err) => {
@@ -177,3 +197,60 @@ export default async function initialize(email, emailVerified, password, userNam
         await prisma.$disconnect()
     })
 }
+
+
+
+
+
+
+    // }).then(async () => {
+    //     //  create subscribe Role
+    //     return await prisma.role.create({
+    //         data: {
+    //             name: 'subscriber',
+    //             description: 'somebody who can only manage their profile.',
+    //             default: true
+    //         }
+    //     })
+    // })
+    // .then(async () => {
+    //     progress.push('Subscribe Role Created.')
+    //     //  create contributor Role
+    //     return await prisma.role.create({
+    //         data: {
+    //             name: 'contributor',
+    //             description: 'somebody who can write and manage their own posts but cannot publish them.'
+    //         }
+    //     })
+    // })
+    // .then(async () => {
+    //     progress.push('Contributer Role Created.')
+    //     //  create author Role
+    //     await prisma.role.create({
+    //             data: {
+    //             name: 'author',
+    //             description: 'somebody who can publish and manage their own posts.'
+    //         }
+    //     })
+    // })
+    // .then(async () => {
+    //     progress.push('Author Role Created.')
+    //     //  create editor Role
+    //     return await prisma.role.create({
+    //         data: {
+    //             name: 'editor',
+    //             description: 'somebody who can publish and manage posts including the posts of other users.'
+    //         }
+    //     })
+    // })
+    // .then(async () => {
+    //     progress.push('Editor Role Created.')
+    //     //  create admin Role
+    //     return await prisma.role.create({
+    //         data: {
+    //             name: 'admin',
+    //             description: 'somebody who has access to all the administration features within a single site.'
+    //         }
+    //     })
+    // })
+    
